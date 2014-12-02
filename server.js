357f858fe439ba123af8d462c0ba4384ace84d5b
bestan <bestan93@gmail.com>
2014-12-02 23:16:25 +0000

    Updated minoval usage

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ var MinoVal = require('minoval');
 var mino = new MinoDB({
     db_address: process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/minodb'
 })
+mino.add_plugin(new MinoVal({user: "testuser"}));
 express()
 	.use(require('body-parser')())
 	.use(express.static('./public'))
 	.use('/mino/', mino.server())
-	.use('/minoval/', new MinoVal(mino).endpoint_server())
 	.get("/get_events", function(req, res) {
-		mino.api.call({"username":"TestUser"},{
+		mino.api.call({"username":"testuser"},{
 		    "function": "search",
 		    "parameters": {
-		        "paths": ["/TestUser/events/"]
+		        "paths": ["/testuser/events/"]
 		    }
 		}, function(req, response) {
 			res.json(response);
