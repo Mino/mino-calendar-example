b7e3a890afbc70bada95b1e1e39fa7422cf17bd0
bestan <bestan93@gmail.com>
2014-12-23 11:55:46 +0200

    Added initial data

---
+++ initial_data.js
@@ -0,0 +1,71 @@
+var logger = require('tracer').console();
+
+module.exports = function(mino, done){
+
+	mino.create_user({
+		"username": "my_app",
+		"email": "marcus+test@minocloud.com",
+		"password": "my_password"
+	}, function(err, res){
+		logger.log(JSON.stringify(err,null,4), res);
+
+		mino.save_type({
+	        "name" : "event",
+	        "display_name" : "Event",
+	        "type" : "object",
+	        "fields" : [ 
+	            {
+	                "name" : "title",
+	                "display_name" : "Title",
+	                "type" : "text",
+	                "min_length" : 2,
+	                "max_length" : null
+	            }, 
+	            {
+	                "name" : "allDay",
+	                "display_name" : "All day",
+	                "type" : "boolean"
+	            }, 
+	            {
+	                "name" : "start",
+	                "display_name" : "Start time",
+	                "type" : "text",
+	                "min_length" : null,
+	                "max_length" : null
+	            }, 
+	            {
+	                "name" : "end",
+	                "display_name" : "End time",
+	                "type" : "text",
+	                "min_length" : null,
+	                "max_length" : null
+	            }
+	        ]
+	    }, function(err, res){
+			logger.log(JSON.stringify(err,null,4), res);
+
+			mino.save([{
+				"name": "events",
+				"folder": true,
+				"path": "/my_app/"
+			}], function(err, res){
+				logger.log(JSON.stringify(err,null,4), res);
+
+				mino.save([{
+					"name": "demo_event",
+					"path": "/my_app/events/",
+					"event": {
+						"title": "MinoVal EVENT",
+						"allDay": true,
+						"start": "2014-12-20",
+						"end": "2014-12-24"
+					}
+				}], function(err, res){
+					logger.log(JSON.stringify(err,null,4), res);
+
+					if(done) done();
+				});
+			});
+		});
+	});
+}
--- server.js
+++ server.js
@@ -2,18 +2,18 @@ var express = require('express');
 var MinoDB = require('minodb');
 var MinoVal = require('minoval');
 var mino = new MinoDB({
-    db_address: process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/minodb'
-})
-mino.add_plugin(new MinoVal({user: "testuser"}));
+    db_address: process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/mino-example'
+}, "my_app");
+require('./initial_data')(mino);
 express()
 	.use(require('body-parser')())
 	.use(express.static('./public'))
 	.use('/mino/', mino.server())
 	.get("/get_events", function(req, res) {
-		mino.api.call({"username":"testuser"},{
+		mino.api.call({"username":"my_app"},{
 		    "function": "search",
 		    "parameters": {
-		        "paths": ["/testuser/events/"]
+		        "paths": ["/my_app/events/"]
 		    }
 		}, function(req, response) {
 			res.json(response);
