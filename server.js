de8f0ddb365c11ab51351c32129afed456795fda
bestan <bestan93@gmail.com>
2015-03-04 14:23:40 +0200

    Using convenience functions for mino calls

--- server.js
+++ server.js
@@ -8,48 +8,27 @@ require('./mino_setup')(function(mino,minoval){
 	.use(express.static('./public'))
 	.use('/mino/', mino.server())
 	.post("/create_event", function(req, res) {
-	
 		minoval.validate("event", req.body, function(err, validator) {
-			logger.log(err);
 			var error = validator.end();
 			if (error) {
-				res.json(error);
-				return;
+				return res.json(error);
 			}
 
-			mino.api.call({username:"my_app"},{
-			    "function": "save",
-			    parameters: {
-			        objects: [
-			            {
-	                        name: req.body.title,
-	                        path: "/my_app/events/",
-	                        event: req.body
-	                    }
-			        ]
-			    }
-			},function(err,response){
-			    logger.log(err, response);
+			var objects = [{
+                name: req.body.title,
+                path: "/my_app/events/",
+                event: req.body
+            }]
+
+			mino.save(objects, function(err,response){
 			    res.json(response);
 			})
-
 		});
-
 	})
 	.get("/get_events", function(req, res) {
-		
-		mino.api.call({username:"my_app"},{
-		    "function": "search",
-		    parameters: {
-		        paths: [
-		            "/my_app/events/"  
-		        ]
-		    }
-		}, function(req, response) {
-			logger.log(req, response);
+		mino.search(["/my_app/events/"], function(req, response) {
 			res.json(response);
 		});
-
 	})
 	.listen(process.env.PORT || 5002);
 });
