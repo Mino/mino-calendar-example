5241ea8a5c77f317e24d2dbc603994fb85b06f0d
bestan <bestan93@gmail.com>
2015-03-09 17:14:10 +0200

    Added slack webhook as static signal

--- mino_setup.js
+++ mino_setup.js
@@ -1,6 +1,8 @@
 var logger = require('tracer').console();
 var MinoDB = require('minodb');
 var MinoVal = require('minoval');
+var StaticSignal = MinoDB.StaticSignal;
+var request = require('request');
 
 module.exports = function(callback){
 
@@ -21,6 +23,24 @@ module.exports = function(callback){
 
 		mino.add_plugin(minoval, function(){
 			require('./initial_data')(mino, minoval, function(){
+				
+				var signal = new StaticSignal({
+					paths: ["/my_app/events/"],
+					include_subfolders: false,
+					handlers: ["save"],
+					callback: function(object) {
+						request({
+							url: "https://zapier.com/hooks/catch/ojbczj/",
+							method: "POST",
+							body: object,
+							json: true
+						}, function(err, httpResponse, body) {
+							logger.log(err, body);
+						})
+					}
+				})
+				mino.signal_manager.add_static_signal(signal);
+
 				callback(mino,minoval);
 			});
 		});
