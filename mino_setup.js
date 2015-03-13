ff3858f12e475a94977e757540c9c733ecab8959
bestan <bestan93@gmail.com>
2015-03-13 15:42:09 +0200

    Removed signals tests

--- mino_setup.js
+++ mino_setup.js
@@ -23,24 +23,6 @@ module.exports = function(callback){
 
 		mino.add_plugin(minoval, function(){
 			require('./initial_data')(mino, minoval, function(){
-				
-				var signal = new StaticSignal({
-					paths: ["/my_app/events/"],
-					include_subfolders: false,
-					handlers: ["save"],
-					callback: function(object) {
-						request({
-							url: "https://zapier.com/hooks/catch/ojbczj/",
-							method: "POST",
-							body: object,
-							json: true
-						}, function(err, httpResponse, body) {
-							logger.log(err, body);
-						})
-					}
-				})
-				mino.signal_manager.add_static_signal(signal);
-
 				callback(mino,minoval);
 			});
 		});
